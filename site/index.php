beca29040b7c6771ff270de4de21c6470a93fe7e
Andrea Morandi <andreamorandi2612@gmail.com>
2022-02-07 11:15:30 +0000

    Added mariadb databases.

--- site/index.php
+++ site/index.php
@@ -1,11 +1,3 @@
 <?php
-    $fileData = json_decode(file_get_contents("data.json"));
-    foreach ($fileData as $indice => $valore) {
-        $d = $valore -> dato;
-        echo "[$indice] $d<br>";
-    }
-    $newValore = new stdClass();
-    $newValore -> dato = ($d+1);
-    $fileData[] = $newValore;
-    file_put_contents("data.json", json_encode($fileData)); 
+    echo "Ciao!<br>";
 ?>
